d720d74fd6f1ed7810508bb3fe17d90904027c08
phulot <phulot@gmail.com>
2015-10-04 23:34:58 +0200

    stat sensibilité/specificité

--- src/main/java/Projet3A/projet/Main.java
+++ src/main/java/Projet3A/projet/Main.java
@@ -28,6 +28,15 @@ public class Main {
 				return new Tuple2<String,Tuple2<Vector,Double>>("id",new Tuple2<Vector,Double>(t._2,t._1));
 			}
 		});
+		long nt = toModel.count();
+		long n = toModel.filter(new Function<Tuple2<String, Tuple2<Vector, Double>>, Boolean>() {
+			public Boolean call(Tuple2<String, Tuple2<Vector, Double>> t) throws Exception {
+				return t._2._2.equals(0d);
+			}
+		}).count();
+		System.out.println(n);
+		System.out.println(nt-n);
+		System.out.println(n/(double)nt+" % de pixels malades");
 		double[] r = new double[]{0.75,0.25};
 		JavaRDD<Tuple2<String,Tuple2<Vector,Double>>>[] sets = toModel.map(new Function<Tuple2<String, Tuple2<Vector, Double>>, Tuple2<String, Tuple2<Vector, Double>>>() {
 					public Tuple2<String, Tuple2<Vector, Double>> call(Tuple2<String, Tuple2<Vector, Double>> t) throws Exception {
@@ -53,19 +62,7 @@ public class Main {
 				return new Tuple2<Double,Double>(model.predict(t._2._1),t._2._2);
 			}
 		});
-		long ntot = results.count();
-//		results.coalesce(3).saveAsTextFile("res");
-//		results.map(new Function<Tuple2<Double, Double>, Object>() {
-//			public Object call(Tuple2<Double, Double> t) throws Exception {
-//				return ""+t._1() +","+t._2();
-//			}
-//		}).saveAsTextFile("results");
-		long nbon = results.filter(new Function<Tuple2<Double, Double>, Boolean>() {
-			public Boolean call(Tuple2<Double, Double> t) throws Exception {
-				return t._1().equals(t._2());
-			}
-		}).count();
-		System.out.println("% de prediction : " + nbon*100/(double)ntot);
+		stat(results);
 	}
 	
 	public static JavaRDD<Tuple2<Double, Vector>> getData(JavaSparkContext sc){
@@ -83,5 +80,36 @@ public class Main {
 			}
 		});
 		return toreturn;
+	}
+	
+	public static void stat(JavaRDD<Tuple2<Double,Double>> res){
+		JavaRDD<Tuple2<Double,Double>> pos = res.filter(new Function<Tuple2<Double, Double>, Boolean>() {
+			public Boolean call(Tuple2<Double, Double> t) throws Exception {
+				return t._2.equals(1d);
+			}
+		});
+		JavaRDD<Tuple2<Double,Double>> neg = res.filter(new Function<Tuple2<Double, Double>, Boolean>() {
+			public Boolean call(Tuple2<Double, Double> t) throws Exception {
+				return t._2.equals(0d);
+			}
+		});
+		long totpos = pos.count();
+		long totneg = neg.count();
+		long tot = totpos+totneg;
+		long totvp = pos.filter(new Function<Tuple2<Double, Double>, Boolean>() {
+			public Boolean call(Tuple2<Double, Double> t) throws Exception {
+				return t._1.equals(t._2);
+			}
+		}).count();
+		long totvn = neg.filter(new Function<Tuple2<Double, Double>, Boolean>() {
+			public Boolean call(Tuple2<Double, Double> t) throws Exception {
+				return t._1.equals(t._2);
+			}
+		}).count();
+		long totv = totvp+totvn;
+		System.out.println("% de prediction : "+totv/(double)tot);
+		System.out.println("% de vp (sensibilité): "+totvp/(double)totpos);
+		System.out.println("% de vn (specificité): "+totvn/(double)totneg);
+		
 	}
 }
